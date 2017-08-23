f9e7cae89f3ee2a8a1f0595ed0454c09c8bd15f7
codeedoc <710987071@qq.com>
2017-08-23 13:53:28 +0800

    i

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react'
 import { BrowserRouter as Router,Link,Route,Redirect,withRouter} from 'react-router-dom';
+//第一部分
 // import Login from './login'
 // import Home from './home'
 // const loggedIn=false;
@@ -16,88 +17,114 @@ import { BrowserRouter as Router,Link,Route,Redirect,withRouter} from 'react-rou
 //
 // export default App;
 
+//第二部分
+// const Public = () => <h3>公开页面</h3>
+// const Video = () => <h3>视频页面333333</h3>
+//
+// const fakeAuth={
+//   isAuthenticated:false,
+//   authenticate(){
+//     this.isAuthenticated=true
+//     console.log(this.isAuthenticated)
+//   }
+// }
+// class Login extends React.Component {
+//   state ={
+//     redirectToReferrer:false
+//   }
+//   login = () => {
+//     fakeAuth.authenticate()
+//     this.setState({
+//       redirectToReferrer:true
+//     })
+//   }
+//   render(){
+//     if(this.props.location.state){
+//       var from=this.props.location.state.from
+//     }else{
+//       var from='/'
+//     }
+//     console.log(this.props.location)
+//     const {redirectToReferrer} = this.state
+//     if(redirectToReferrer){
+//       return(
+//         <Redirect to={from}/>
+//       )
+//     }
+//     return(
+//       <div>
+//         <button onClick={this.login}>登录</button>
+//       </div>
+//     )
+//   }
+// }
+//
+// const AuthButton = withRouter(() => (
+//   fakeAuth.isAuthenticated ? (
+//     <p>你好！</p>
+//   ):
+//   (<p>尚未登录</p>)
+//  )
+// )
+//
+// const PrivateRoute =({component:Component,...rest}) => (
+//   <Route {...rest} render={(props)=>(
+//       fakeAuth.isAuthenticated ?
+//       (<Component />) : (
+//         <Redirect to={{
+//             pathname:'/login',
+//             state:{from:props.location}
+//           }}/>
+//       )
+//     )}/>
+// )
+//
+// class App extends React.Component{
+//
+//   render(){
+//
+//     return(
+//       <Router>
+//         <div>
+//           <AuthButton/>
+//           <ul>
+//             <li><Link to='/public'>公开页面</Link></li>
+//             <li><Link to='/video'>视频</Link></li>
+//             <li><Link to='/login'>登录</Link></li>
+//           </ul>
+//           <Route path='/public' component={Public}/>
+//           <Route path='/login' component={Login}/>
+//           <PrivateRoute path='/video' component={Video}/>
+//         </div>
+//       </Router>
+//     )
+//   }
+// }
+//
+// export default App
 
-const Public = () => <h3>公开页面</h3>
-const Video = () => <h3>视频页面333333</h3>
 
-const fakeAuth={
-  isAuthenticated:false,
-  authenticate(){
-    this.isAuthenticated=true
-    console.log(this.isAuthenticated)
-  }
-}
-class Login extends React.Component {
-  state ={
-    redirectToReferrer:false
-  }
-  login = () => {
-    fakeAuth.authenticate()
-    this.setState({
-      redirectToReferrer:true
-    })
-  }
-  render(){
-    if(this.props.location.state){
-      var from=this.props.location.state.from
-    }else{
-      var from='/'
-    }
-    console.log(this.props.location)
-    const {redirectToReferrer} = this.state
-    if(redirectToReferrer){
-      return(
-        <Redirect to={from}/>
-      )
-    }
-    return(
-      <div>
-        <button onClick={this.login}>登录</button>
-      </div>
-    )
-  }
-}
+//第三部分
 
-const AuthButton = withRouter(() => (
-  fakeAuth.isAuthenticated ? (
-    <p>你好！</p>
-  ):
-  (<p>尚未登录</p>)
- )
+
+const Video1 = () =>(
+  <div>Git</div>
 )
-
-const PrivateRoute =({component:Component,...rest}) => (
-  <Route {...rest} render={(props)=>(
-      fakeAuth.isAuthenticated ?
-      (<Component />) : (
-        <Redirect to={{
-            pathname:'/login',
-            state:{from:props.location}
-          }}/>
-      )
-    )}/>
+const Video2 = () => (
+  <div>React</div>
 )
-
-class App extends React.Component{
-
-  render(){
-
-    return(
-      <Router>
-        <div>
-          <AuthButton/>
-          <ul>
-            <li><Link to='/public'>公开页面</Link></li>
-            <li><Link to='/video'>视频</Link></li>
-            <li><Link to='/login'>登录</Link></li>
-          </ul>
-          <Route path='/public' component={Public}/>
-          <Route path='/login' component={Login}/>
-          <PrivateRoute path='/video' component={Video}/>
-        </div>
-      </Router>
-    )
-  }
-}
+const App = () =>(
+  <Router>
+    <div>
+      <h2>Accounts</h2>
+      <ul>
+        <li><Link to='/v/1-git'>1-git</Link></li>
+        <li><Link to='/v/2-react'>2-react</Link></li>
+      </ul>
+      <Route path='/v/1-git' compoment={Video1} />
+      <Route path='/v/2-react' compoment={Video2} />
+    </div>
+  </Router>
+)
 
 export default App
